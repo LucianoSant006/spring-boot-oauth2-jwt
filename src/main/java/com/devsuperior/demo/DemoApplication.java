e37af1739d578b56970e88c9d3062879241cb691
LucianoSant006 <santiagoluciano383@gmail.com>
2024-12-11 00:40:05 -0300

    Spring Security Checklist,not tested

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication implements CommandLineRunner  {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(passwordEncoder.encode("123456"));
-		
+
 	}
 
 	public static void main(String[] args) {

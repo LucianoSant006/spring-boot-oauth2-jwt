9dcb2d525a68fb200316bcb36faaf5d84e521854
LucianoSant006 <santiagoluciano383@gmail.com>
2024-12-10 23:35:20 -0300

    BCrypt password

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -4,12 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
-public class DemoApplication  {
+public class DemoApplication implements CommandLineRunner  {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(passwordEncoder.encode("123456"));
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
